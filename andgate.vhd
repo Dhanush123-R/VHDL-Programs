d29adc8f263b90a57e04e7b75ea7bb247afeafe8
Dhanush123-R <111369390+Dhanush123-R@users.noreply.github.com>
2024-10-11 20:12:11 +0530

    andgate.vhd

--- andgate.vhd
+++ andgate.vhd
@@ -1,1 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
 
+entity andgt is
+port(
+A : in std_logic;   
+B : in std_logic;      
+Y : out std_logic
+);
+end andgt;
+
+architecture dataflow of andgt is
+ begin
+Y <= A AND B;
+end dataflow; 
